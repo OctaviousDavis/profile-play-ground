7004a037be7a1efb28e41b1e1371a3a0e067db19
davis <davisoctavious83@gmail.com>
2023-11-21 15:57:49 -0800

    "finally"

---
+++ js/snake.js
@@ -0,0 +1,107 @@
+let blockSize = 25;
+let rows = 20;
+let cols = 20;
+let board;
+let context;
+
+
+let snakeX = blockSize * 5
+let snakeY = blockSize * 5
+let velocityX = 0;
+let velocityY = 0;
+
+let foodX;
+let foodY;
+
+let snakeBody = [];
+
+let gameOver = false;
+
+function update () {
+    if (gameOver){
+       return;
+    }
+    context.fillStyle = 'black';
+    context.fillRect(0,0 ,board.width, board.height);
+
+    context.fillStyle = 'red'
+    context.fillRect(foodX,foodY,blockSize,blockSize)
+
+    if(snakeX === foodX && snakeY === foodY) {
+       snakeBody.push([foodX,foodY]);
+       context.clearRect(foodX, foodY, blockSize, blockSize)
+       placeFood();
+    }
+
+    for (let i = snakeBody.length-1; i > 0; i--){
+       snakeBody[i] = snakeBody[i-1]
+    }
+    if (snakeBody.length){
+       snakeBody[0] = [snakeX, snakeY]
+    }
+
+    context.fillStyle = 'lime';
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize)
+    for (let i = 0; i < snakeBody.length; i++){
+       context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
+       gameOver = true;
+       alert('game over')
+    }
+    for(let i = 0; i < snakeBody.length; i ++){
+       if (snakeX === snakeBody[i][0] && snakeY === snakeBody[i][1]){
+          gameOver = true;
+          alert('game over')
+       }
+    }
+}
+const foodPlacement = setTimeout(() => {
+    placeFood();
+    console.log('foodplacement')
+}, 10000);
+if (snakeX === foodX && snakeY === foodY) {
+    clearTimeout(foodPlacement)
+    console.log('cleared')
+}
+function placeFood() {
+    if (gameOver) {
+       return
+    }
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+    console.log('placefood')
+}
+
+
+function changeDirection (e) {
+    if(e.code === 'ArrowUp' && velocityY !== 1){
+       velocityX = 0;
+       velocityY =    -1;
+    }
+    else if(e.code === 'ArrowDown' && velocityY !== -1){
+       velocityX = 0;
+       velocityY = 1;
+    }
+    else if(e.code === 'ArrowLeft' && velocityX !== 1){
+       velocityX = -1
+       velocityY = 0
+    }
+    else if(e.code === 'ArrowRight' && velocityX !== -1){
+       velocityX = 1
+       velocityY = 0
+    }
+}
+
+
+(()=>{
+    board = document.querySelector('#board')
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext('2d')
+    placeFood();
+    document.addEventListener("keydown",changeDirection)
+    setInterval(update, 1000/10);
+})();
